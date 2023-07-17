dd4ad4764420baa28160be9f79c7c731f1da43ac
Zade Viggers <zade@viggers.net>
2023-07-18 11:04:37 +1200

    Basic indicator works

--- 13dts_project.ino
+++ 13dts_project.ino
@@ -1,12 +1,24 @@
+// Pins
 const int MOISTURE_SENSOR_SIGNAL = 14;
 const int MOISTURE_SENSOR_POWER = 12;
+const int NEEDS_WATERING_INDICATOR = 13;
 
-volatile int moisture = 69;
+// Constants
+const int NEEDS_WATERING_THRESHOLD = 200;
+
+// Variables
+volatile int moisture = 0;
+
+
 
 void setup() {
+  // For debugging
   Serial.begin(9600);
+
+  // Setup pins
   pinMode(MOISTURE_SENSOR_POWER, OUTPUT);
   pinMode(MOISTURE_SENSOR_SIGNAL, INPUT);
+  pinMode(NEEDS_WATERING_INDICATOR, OUTPUT);
 }
 
 void loop() {
@@ -18,7 +30,15 @@ void loop() {
   // Turn off the sensor to reduce metal corrosion over time
   digitalWrite(MOISTURE_SENSOR_POWER, LOW);
 
+  // For debugging
   Serial.println(moisture);
+
+  // Temporay LED indicator
+  if (moisture < NEEDS_WATERING_THRESHOLD) {
+    digitalWrite(NEEDS_WATERING_INDICATOR, HIGH);
+  } else {
+    digitalWrite(NEEDS_WATERING_INDICATOR, LOW);
+  }
 
 
   delay(1000);
