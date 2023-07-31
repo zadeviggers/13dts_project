5b1447626533354fdc3075243e06adc9d58118e0
Zade Viggers <zade@viggers.net>
2023-07-31 12:00:53 +1200

    Clean up

--- 13dts_project.ino
+++ 13dts_project.ino
@@ -9,9 +9,8 @@ const int NEEDS_WATERING_INDICATOR = 13;
 const int ULTRASONIC_TRIGGER = 27;
 const int ULTRASONIC_ECHO = 26;
 const int LCD_ADDRESS = 0x27;
-const int SCALE_POT = 25;
-const int OFFSET_POT = 33;
-const int THRESHOLD_POT = 32;
+const int MOSITURE_MAX_POT = 25;
+const int MOISTURE_OFFSET_POT = 33;
 
 // Constants
 const int DISTANCE_THRESHOLD = 10;
@@ -33,11 +32,10 @@ byte GRAPH_SEGMENT_SHAPE[8] = {
 int GRAPH_SEGMENT = 0;  // Chacter ID
 
 // Variables
-volatile int moisture = 0;             // Moisture sensor
-volatile float distance = 0;           // Ultrasonic sensor
-volatile int moisture_max = 200;       // Potentiomiter
-volatile int moisture_multiplier = 1;  // Potentiomiter
-volatile int moisture_offset = 0;      // Potentiomiter
+volatile int moisture = 0;         // Moisture sensor
+volatile float distance = 0;       // Ultrasonic sensor
+volatile int moisture_max = 200;   // Potentiomiter
+volatile int moisture_offset = 0;  // Potentiomiter
 volatile bool potentiomiters_changing = false;
 
 // Inititalise libraries
@@ -55,9 +53,8 @@ void setup() {
 
   pinMode(NEEDS_WATERING_INDICATOR, OUTPUT);
 
-  pinMode(SCALE_POT, INPUT);
-  pinMode(OFFSET_POT, INPUT);
-  pinMode(THRESHOLD_POT, INPUT);
+  pinMode(MOISTURE_OFFSET_POT, INPUT);
+  pinMode(MOSITURE_MAX_POT, INPUT);
 
   // Setup LCD
   lcd.begin();
@@ -71,21 +68,18 @@ bool is_pot_change_over_threshold(int a, int b) {
 
 void loop() {
   // Check potentiomiter values
-  int _moisture_multiplier = analogRead(SCALE_POT);
-  int _moisture_offset = analogRead(OFFSET_POT);
-  int _mositure_threshold = analogRead(THRESHOLD_POT);
+  int _moisture_min = analogRead(MOISTURE_OFFSET_POT);
+  int _mositure_max = analogRead(MOSITURE_MAX_POT);
   if (
-    is_pot_change_over_threshold(_moisture_multiplier, moisture_multiplier)
-    || is_pot_change_over_threshold(_moisture_offset, moisture_offset)
-    || is_pot_change_over_threshold(_mositure_threshold, moisture_max)) {
+    is_pot_change_over_threshold(_moisture_min, moisture_offset)
+    || is_pot_change_over_threshold(_mositure_max, moisture_max)) {
     potentiomiters_changing = true;
+    moisture_offset = _moisture_min;
+    moisture_max = _mositure_max;
   } else {
     potentiomiters_changing = false;
   }
-  moisture_multiplier = _moisture_multiplier;
-  if (moisture_multiplier == 0) moisture_multiplier = 1;
-  moisture_offset = _moisture_offset;
-  moisture_max = _mositure_threshold;
+
 
 
   // Check distance on ultrasonic
@@ -102,55 +96,58 @@ void loop() {
     // Turn off the sensor to reduce metal corrosion over time
     digitalWrite(MOISTURE_SENSOR_POWER, LOW);
 
+    // Caculate computed moisture
+    int computed_moisture = moisture + moisture_offset;
+
 
     // Turn on the LCD backlight
-    // lcd.backlight();
+    lcd.backlight();
     lcd.clear();
 
     // If potentiomiters are changing, show them on the screen. Otherwise, show data.
     if (potentiomiters_changing) {
+      // Draw moisture offset on the left
       lcd.setCursor(0, 0);
-      lcd.print(moisture_multiplier);
-      lcd.print(F(" "));
       lcd.print(moisture_offset);
-      lcd.print(F(" "));
+
+      // Draw moisture threshold on right
+      lcd.setCursor(LCD_WIDTH - String(moisture_max).length(), 0);
       lcd.print(moisture_max);
     } else {
-
       // Display message on LCD
-      if (moisture < moisture_max) {
+      if (computed_moisture < moisture_max) {
         // LED indicator
         digitalWrite(NEEDS_WATERING_INDICATOR, HIGH);
 
         // Print to LCD. `F()` macro makes the string get stored in flash memory rather than RAM.
-        lcd.print(F("WATER ME"));
+        lcd.print(F("Low moisture!"));
 
 
         // lcd.print("WATER ME");
       } else {
         // Print to LCD. `F()` macro makes the string get stored in flash memory rather than RAM.
-        lcd.print(F("DON'T WATER"));
+        lcd.print(F("Moisture OK"));
         // lcd.print("DON'T WATER");
       }
     }
 
+    // TODO: Change this to middle of bottom line
     // Draw the mositure on the right of the top line
-    String moisture_str = String(moisture);
-    lcd.setCursor(LCD_WIDTH - moisture_str.length(), 0);
-    lcd.print(moisture);
+    // String moisture_str = String(moisture);
+    // lcd.setCursor(LCD_WIDTH - moisture_str.length(), 0);
+    // lcd.print(moisture);
 
     // Draw the line graph
-    if (moisture >= moisture_offset || moisture <= moisture_max) {
-      float moisture_percentage = (moisture + moisture_offset) / moisture_max;
-      int cells_to_fill = LCD_WIDTH * moisture_percentage;
+    float moisture_percentage = (moisture + moisture_offset) / moisture_max + 1;  // Avoid dividing by zero
+    int cells_to_fill = LCD_WIDTH * moisture_percentage;
 
-      lcd.setCursor(0, 1);
-      for (int i = 0; i < LCD_WIDTH; i++) {
-        if (i < cells_to_fill) {
-          lcd.write(GRAPH_SEGMENT);
-        }
+    lcd.setCursor(0, 1);
+    for (int i = 0; i < LCD_WIDTH; i++) {
+      if (i < cells_to_fill) {
+        lcd.write(GRAPH_SEGMENT);
       }
     }
+
 
   } else {
     // turn off the LED
